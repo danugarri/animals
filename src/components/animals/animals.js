c3e17e5d85ba148799b5d6680d56b4d0c360981c
daniel <danugarri@gmail.com>
2021-06-16 13:46:19 +0200

    add some modifications and styles

---
+++ src/components/animals/animals.js
@@ -0,0 +1,14 @@
+export const animals = {
+    dolphin: {
+      image: 'https://www.ecoticias.com/userfiles/extra/HTSJ_hibridacio.jpg',
+      facts: ['El delfín es uno de los animales más inteligentes del planeta', 'Existen muchas especies y todas son capaces de comunicarse entre sí', 'Los expertos afirman que estos animales pueden ayudar a curar enfermedades.']
+    },
+    lobster: {
+      image: "https://sobrehistoria.com//wp-content/uploads/2017/02/por-que-la-langosta-se-convirtio-en-un-alimento-de-lujo-plaga-600x463.jpg",
+      facts: ['Las langostas son casi ciegas,Pero tienen un increíble sentido del olfato.', 'Una tenaza siempre es más grande que la otra', 'La sangre de langosta es incolora.']
+    },
+    starfish: {
+      image: 'https://www.lifeder.com/wp-content/uploads/2019/06/estrella-de-mar-1.jpg',
+      facts: ['Esto no es un mito: los brazos de las estrellas de mar se regeneran', 'La estrellas de mar no tienen sangre', 'Las estrellas de mar tienen un ojo al final de cada brazo ']
+    }
+  };
--- src/index.js
+++ src/index.js
@@ -1,17 +1,62 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { animals } from '../src/components/animals/animals.js';
+import './styles.css';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+
+
+//variables
+const title='Selecciona un animal para ver un dato curioso';
+const background=( <img className='background' src='https://imagenes.20minutos.es/files/image_656_370/uploads/imagenes/2019/03/03/816158.jpg' alt='ocean'/>);
+const images=[];
+for( let animal in animals){
+  images.push(
+    <img 
+    key={animal}
+    className='animal'
+    alt={animal}
+    src={animals[animal].image} 
+    ariaLabel={animal}
+    role='button'
+    onClick={displayFact}
+    />
+    )
+    };
+//boolean que hace que se muestre o no el background
+const showBackground=true;
+
+/* tenemos varias opciones para jugar con el título que mostramos dentro del h1.
+*opción 1: ternary operator
+  {title ==='' ? 'Click an animal for a fun fact' : title}
+  
+  *opcion 2 : OR operator. el primer argunmento se muetra si se evalua como true, sino se muestra el segundo*/
+const animalFacts= (
+  <div>
+    <h1>
+      {title || 'Click an animal for a fun fact'}
+    </h1>
+    {showBackground && background}
+    <p id ='fact'></p>
+    <div className='animals'>
+    {images}
+    </div>
+</div>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+/*If showBackground is true, background should show up. If it’s false, it should not. Use the && operator in animalFacts to implement this feature.*/
+
+//  ** event handler function **
+//ojo porque si almacenas la fución como una arrow function en una variable no funciona (aunque la declares con let)
+function displayFact (e) {
+const selectedAnimal= e.target.alt;
+const animalInfo = animals[selectedAnimal];
+const randomIndex= Math.floor(Math.random()*animalInfo.facts.length)
+const funFact= animalInfo.facts[randomIndex];
+document.getElementById('fact').innerHTML=funFact;
+}
+
+//rendering on screem
+ReactDOM.render(
+  animalFacts,
+  document.getElementById('root')
+  )
